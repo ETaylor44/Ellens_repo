9ab17da75a8f055388181ec75ade7e372c2cb8db
Ellen Taylor <>
2024-06-24 08:25:05 +0100

    Added function to print riffs and scales to a stave.

--- music_note_programme.py
+++ music_note_programme.py
@@ -2,8 +2,11 @@ import random
 
 riff = []
 sharp_notes = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"]
+flat_notes = ["A", "Bb", "B", "C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab"]
 user_number_of_notes = "How many notes do you want in the riff? "
 not_a_number_statement = "Please enter a number."
+stave_space = ""
+stave_line = ""
 invalid_character = True
 maj_scale = [0, 2, 4, 5, 7, 9, 11]
 min_scale = [0, 2, 3, 5, 7, 8, 11]
@@ -30,13 +33,44 @@ def initialse_riff(riff, note_list, number_of_notes):
         random_number = random.randrange(0, len(note_list)-1)
         riff.append(note_list[random_number])
     print(riff)
+    return riff
+
+def print_notes_horizontal_on_stave(list_of_notes, natural_notes, stave_line, stave_space):
+    for i in range(0, len(natural_notes)):
+        stave_line = ""
+        stave_space = ""
+        for j in range(0, len(list_of_notes)):
+            print_line = False
+            if natural_notes[i] == list_of_notes[j][0]:
+                if i % 2 == 1:
+                    print_line = True
+                    if len(list_of_notes[j]) == 2:
+                        stave_line += "--o#--"
+                    else:
+                        stave_line += "--o---"
+                else:
+                    if len(list_of_notes[j]) == 2:
+                        stave_space += "  o#  "
+                    else:
+                        stave_space += "  o   "
+            else:
+                if i % 2 == 1:
+                    print_line = True
+                    stave_line += "------"
+                else:
+                    stave_space += "      "
+        if print_line == False:
+            print(stave_space)
+        else:
+            print(stave_line) 
+
 
 # Function to check that user input is a musical note.
 def get_note_input(user_input_prompt, error_message):
     while True:
         user_note = input(user_input_prompt)
         for i in range(0, len(sharp_notes)):
-            if user_note.lower() == sharp_notes[i].lower():
+            if user_note.lower() == sharp_notes[i].lower() or user_note.lower() == flat_notes[i].lower():
                 return user_note
         print(error_message)
 
@@ -65,9 +99,15 @@ def print_stave(natural_triad, list_of_notes):
             notes_printed += 1
             if len(list_of_notes[i]) == 2:
                 if j % 2 == 0:
-                    print("  o#")
+                    if list_of_notes[i][1] == "#":
+                        print("  o#")
+                    else:
+                        print("  ob")
                 else:
-                    print("--o#--")
+                    if list_of_notes[i][1] == "#":
+                        print("--o#--")
+                    else:
+                        print("--ob--")
             else:
                 if j % 2 == 0:
                     print("  o")
@@ -109,70 +149,54 @@ def create_key(user_input_note_index, notes, is_major_key, user_note):
         new_key.append(notes[note_position])
     print(statement)
     print(new_key)
+    return new_key
 
 # Get major or minor key from user.
 def get_user_input_maj_min(input_message, error_message):
     while True:
         user_key_input = input(input_message)
         if user_key_input.lower() == "major" or user_key_input.lower() == "maj":
-            return user_key_input
+            return True
         elif user_key_input.lower() == "minor" or user_key_input.lower() == "min":
-            return user_key_input
+            return False
         else:
             print(error_message)
 
-# Function to call triad function or create key function.
-def user_key_input_func(user_key_input, call_triad_func):
-    if user_key_input.lower() == "major" or user_key_input == "maj":
-        is_major_key = True
-        if call_triad_func == False:
-            create_key(user_input_note_index, sharp_notes, is_major_key, user_note)
-        else:
-            create_chords(user_note, sharp_notes, is_major_key)
-    elif user_key_input.lower() == "minor" or user_key_input == "min":
-        is_major_key = False
-        if call_triad_func == False:
-            create_key(user_input_note_index, sharp_notes, is_major_key, user_note)
-        else:
-            create_chords(user_note, sharp_notes, is_major_key)
 
 # Function to construct a major or minor triad.
 def create_chords(user_note, note_list, is_major_key):
     triad = []
-    not_a_note = True
-    for j in range(0, len(note_list)):
-        if user_note.lower() == note_list[j].lower():
-            not_a_note = False
-            user_note_index = j
-            if is_major_key == True:
-                statement = f"Notes of the {user_note.title()} Major triad:"
-                third_index = user_note_index + 4
-                if third_index >= 12:
-                    third_index -= 12
-                third = note_list[third_index]
-            else:
-                statement = f"Notes of the {user_note.title()} Minor triad:"
-                third_index = user_note_index + 3
-                if third_index >= 12:
-                    third_index -= 12
-                third = note_list[third_index]
-            fifth_index = user_note_index + 7
-            if fifth_index >= 12:
-                fifth_index -= 12
-            fifth = note_list[fifth_index]
-            triad.extend([note_list[user_note_index], third, fifth])
-            print(statement)
-            print(triad)
-            check_for_sharp(triad)
-    if not_a_note == True:
-        print("This isn't a valid musical note.")
+    while True:
+        for j in range(0, len(note_list)):
+            if user_note.lower() == note_list[j].lower():
+                user_note_index = j
+                if is_major_key == True:
+                    statement = f"Notes of the {user_note.title()} Major triad:"
+                    third_index = user_note_index + 4
+                    if third_index >= 12:
+                        third_index -= 12
+                    third = note_list[third_index]
+                else:
+                    statement = f"Notes of the {user_note.title()} Minor triad:"
+                    third_index = user_note_index + 3
+                    if third_index >= 12:
+                        third_index -= 12
+                    third = note_list[third_index]
+                fifth_index = user_note_index + 7
+                if fifth_index >= 12:
+                    fifth_index -= 12
+                fifth = note_list[fifth_index]
+                triad.extend([note_list[user_note_index], third, fifth])
+                print(f"{statement}\n{triad}")
+                return triad
         
 
-
+# Script:
 #1 riff
 print("This programme will generate random notes to create a riff.")
 user_input = get_numeric_input(user_number_of_notes, not_a_number_statement)
 initialse_riff(riff, sharp_notes, user_input)
+print_notes_horizontal_on_stave(riff, natural_notes, stave_line, stave_space)
 
 #2 scale
 print("\nThis programme will generate the notes of any major or minor key.")
@@ -182,9 +206,13 @@ list_of_notes = convert_user_note_to_list(user_note, list_of_notes)
 check_for_sharp(list_of_notes)
 
 call_triad_func = False
-user_key_input = user_key_input = get_user_input_maj_min(user_key_prompt, invalid_key_message)
+is_major_key = get_user_input_maj_min(user_key_prompt, invalid_key_message)
 
-user_key_input_func(user_key_input, call_triad_func)
+if call_triad_func == False:
+    new_key = create_key(user_input_note_index, sharp_notes, is_major_key, user_note)
+
+print_notes_horizontal_on_stave(new_key, natural_notes, stave_line, stave_space)
+
 
 # 3 Triad 
 print("\nThis programme will construct a Major or Minor triad.")
@@ -192,9 +220,12 @@ print("\nThis programme will construct a Major or Minor triad.")
 user_input = get_note_input(starting_note_for_triad, invalid_note_message)
 
 call_triad_func = True
-user_key_input = get_user_input_maj_min(user_key_prompt, invalid_key_message)
+is_major_key = get_user_input_maj_min(user_key_prompt, invalid_key_message)
 
-user_key_input_func(user_key_input, call_triad_func)
+if call_triad_func == True:
+    triad = create_chords(user_input, sharp_notes, is_major_key)
+
+check_for_sharp(triad)
 
 
 
