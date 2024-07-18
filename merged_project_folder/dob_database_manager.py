73c2f10379e918df78424d66fa0f2a711ace8e40
Ellen Taylor <>
2024-07-18 05:34:45 +0100

    First commit from new branch

--- merged_project_folder/dob_database_manager.py
+++ merged_project_folder/dob_database_manager.py
@@ -5,6 +5,14 @@ def dob_database_manager_func():
 
     month_list = ["January", "February", "March", "April", "May", "June", 
                 "July", "August", "September", "October", "November", "December"]
+    
+    def is_txt_file_open():
+        try:
+            f = open("dob.txt", "r")
+            is_file_open = True
+        except Exception as e:
+            is_file_open = False
+        return is_file_open
 
     # Define function to find data and return as list..
     def find_function():
@@ -59,116 +67,122 @@ def dob_database_manager_func():
     
 
     # Script.
+    is_file_open = is_txt_file_open()
+    if is_file_open == False:
+        print("Error. Please open the Date of Birth Database.")
+    else:
     # Get valid user input.
-    while True:
-        user_input = input('''\nPlease select one of the following options:
-            f - find information
-            a - add information
-            r - remove information
-            e - exit date of birth database manager
+        while True:
+            user_input = input('''\nPlease select one of the following options:
+                f - find information
+                a - add information
+                r - remove information
+                e - exit date of birth database manager
 
-            Enter selection: ''')
-        match user_input:
+                Enter selection: ''')
+            match user_input:
 
-            # Find details.
-            case "f":
-                # Get valid first name from user.
-                details_as_list = find_function()
-                # Ask which details to display.
+                # Find details.
+                case "f":
+                    # Get valid first name from user.
+                    details_as_list = find_function()
+                    # Ask which details to display.
 
-                while True:
-                    user_choice = input('''\nWould you like to view:
-    f - full name
-    i - initials
-    d - date of birth
-    a - age
-    ad - all details
+                    while True:
+                        user_choice = input('''\nWould you like to view:
+        f - full name
+        i - initials
+        d - date of birth
+        a - age
+        ad - all details
 
-    Enter selection: ''')
-                    match user_choice:
-                        # Display full name.
-                        case "f":
-                            print(f"\nFull name: {details_as_list[0]} {details_as_list[1]}")   
-                            break   
-                        # Display dob (dd/mm/yyyy)      
-                        case "d":
-                            month_number = convert_month_word_to_number(details_as_list)
-                            print(f"\nDate of birth: {str(details_as_list[2])}/{str(month_number)}/{str(details_as_list[4])}")
+        Enter selection: ''')
+                        match user_choice:
+                            # Display full name.
+                            case "f":
+                                print(f"\nFull name: {details_as_list[0]} {details_as_list[1]}")   
+                                break   
+                            # Display dob (dd/mm/yyyy)      
+                            case "d":
+                                month_number = convert_month_word_to_number(details_as_list)
+                                print(f"\nDate of birth: {str(details_as_list[2])}/{str(month_number)}/{str(details_as_list[4])}")
+                                break
+                            # Display initals.
+                            case "i":
+                                print(f"\nInitials: {details_as_list[0][0]}{details_as_list[1][0]}")
+                                break
+                            # Display age. Not working correctly
+                            case "a":
+                                month_number = convert_month_word_to_number(details_as_list)
+                                formatted_dob = datetime.datetime(int(details_as_list[4]), int(month_number), int(details_as_list[2]))
+                                age = calculate_age(formatted_dob)
+                                print(f"\nAge: {age} years old.")
+                            # Display all details as string.
+                            case "ad":
+                                details_as_string = " ".join(details_as_list)
+                                print(f"\n{details_as_string}")
+                                break
+                            case _:
+                                print("Not a valid input.")
+
+
+                # Add details.
+                case "a":
+                    all_lines = ""
+                    f = open("DOB.txt", "r")
+                    for line in f:
+                        all_lines += line
+
+                    # TO DO: enter first name, surname, date, month, year as separate inputs.
+                    while True:
+                        user_data_input = input("\nPlease enter 'firstname surname date month year' in this format: ")
+                        user_data_input_titled = user_data_input.title()
+                        user_data_input_as_list = user_data_input_titled.split(" ")
+                        if len(user_data_input_as_list) == 5:
+                            with open("DOB.txt", "w") as f:
+                                f.write(all_lines + "\n" + user_data_input_titled)
+                            print(f"\n\033[1m{user_data_input_titled}\033[0m has been Successfully added to the database")
                             break
-                        # Display initals.
-                        case "i":
-                            print(f"\nInitials: {details_as_list[0][0]}{details_as_list[1][0]}")
-                            break
-                        # Display age. Not working correctly
-                        case "a":
-                            month_number = convert_month_word_to_number(details_as_list)
-                            formatted_dob = datetime.datetime(int(details_as_list[4]), int(month_number), int(details_as_list[2]))
-                            age = calculate_age(formatted_dob)
-                            print(f"\nAge: {age} years old.")
-                        # Display all details as string.
-                        case "ad":
-                            details_as_string = " ".join(details_as_list)
-                            print(f"\n{details_as_string}")
-                            break
-                        case _:
-                            print("Not a valid input.")
+                        else:
+                            print("\nPlease make sure to use the specified formatting.")
 
 
-            # Add details.
-            case "a":
-                all_lines = ""
-                f = open("DOB.txt", "r")
-                for line in f:
-                    all_lines += line
-
-                # TO DO: enter first name, surname, date, month, year as separate inputs.
-                while True:
-                    user_data_input = input("\nPlease enter 'firstname surname date month year' in this format: ")
-                    user_data_input_titled = user_data_input.title()
-                    user_data_input_as_list = user_data_input_titled.split(" ")
-                    if len(user_data_input_as_list) == 5:
+                # Remove details.
+                case "r":
+                    line_to_remove = get_valid_name_to_remove()
+                    remove_choice = confirm_removal(line_to_remove)
+                    line_to_remove_as_list = line_to_remove.split(" ")
+                    if remove_choice == "y":
+                        with open("DOB.txt", "r") as f:
+                            lines = f.readlines()
                         with open("DOB.txt", "w") as f:
-                            f.write(all_lines + "\n" + user_data_input_titled)
-                        print(f"\n\033[1m{user_data_input_titled}\033[0m has been Successfully added to the database")
-                        break
+                            for line in lines:
+                                if line.strip("\n") != line_to_remove_as_list.strip("\n"):
+                                    f.write(line)
+                        print(f"\n{line_to_remove_as_list[0].title()} {line_to_remove_as_list[1].title()}"
+                            " has been removed from the file.")
                     else:
-                        print("\nPlease make sure to use the specified formatting.")
+                        print(f"\n{line_to_remove_as_list[0].title()} {line_to_remove_as_list[1].title()}"
+                            " has not been removed from the file.")
+                
+                case "e":
+                    print("\nYou are exiting the DOB database manager.")
+                    break
+                            
+                case _:
+                    print("\nYou did not enter a valid option.")
 
+        #function GetUserStringInput (string question)
+        #   ask question
+        #   get valid user input (while loop)
+        #   return user input
 
-            # Remove details.
-            case "r":
-                line_to_remove = get_valid_name_to_remove()
-                remove_choice = confirm_removal(line_to_remove)
-                line_to_remove_as_list = line_to_remove.split(" ")
-                if remove_choice == "y":
-                    with open("DOB.txt", "r") as f:
-                        lines = f.readlines()
-                    with open("DOB.txt", "w") as f:
-                        for line in lines:
-                            if line.strip("\n") != line_to_remove_as_list.strip("\n"):
-                                f.write(line)
-                    print(f"\n{line_to_remove_as_list[0].title()} {line_to_remove_as_list[1].title()}"
-                        " has been removed from the file.")
-                else:
-                    print(f"\n{line_to_remove_as_list[0].title()} {line_to_remove_as_list[1].title()}"
-                        " has not been removed from the file.")
-            
-            case "e":
-                print("\nYou are exiting the DOB database manager.")
-                break
-                           
-            case _:
-                print("\nYou did not enter a valid option.")
+        #function GetUserIntInput (string question)
+        #   same as previous, but validate for int
 
-    #function GetUserStringInput (string question)
-    #   ask question
-    #   get valid user input (while loop)
-    #   return user input
+        #function GetAllInfoForOnePerson(int id)
+        #   firstName = GetUserStringInput("What is the first name?")
+        #   secondName = GetUserStringInput("What is the second name?")
+        #   age = GetUserIntInput("What is the age?")
 
-    #function GetUserIntInput (string question)
-    #   same as previous, but validate for int
-
-    #function GetAllInfoForOnePerson(int id)
-    #   firstName = GetUserStringInput("What is the first name?")
-    #   secondName = GetUserStringInput("What is the second name?")
-    #   age = GetUserIntInput("What is the age?")
+dob_database_manager_func()
